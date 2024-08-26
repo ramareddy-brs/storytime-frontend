3111b9efc3c3ae3c5133c4e4fd3f0bb0398eb489
Ramareddy <ramareddy9652@gmail.com>
2024-08-26 10:44:22 +0530

    changes done

--- src/store/apiSlice.js
+++ src/store/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:5000",
+  baseUrl: "https://backend-storytime.onrender.com",
   prepareHeaders: (headers, { getState }) => {
     const state = getState();
  
